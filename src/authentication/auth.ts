a91ce05a8f3f388d863f17b1b556336419f947a5
Andre Barsotti <andre.barsotti@accelera.com.br>
2020-10-21 18:02:42 -0300

    Incluindo autenticação na página

---
+++ src/authentication/auth.ts
@@ -0,0 +1,36 @@
+import { Handler } from 'express';
+import passport from 'passport';
+import { ExtractJwt, Strategy, StrategyOptions, VerifiedCallback } from "passport-jwt";
+import { env } from 'process';
+import { Logger } from 'tslog';
+import { UsuarioRepository } from '../repositories/usuario-repository';
+
+export class Auth {
+  private log = new Logger();
+  private params: StrategyOptions = {
+    secretOrKey: env['JwtSecret'] || '',
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+  }
+  private repo = new UsuarioRepository();
+
+  private strategy = new Strategy(this.params, (payload: any, done: VerifiedCallback) => {
+    this.log.debug(`Validando o usuário ${payload.userName}`)
+    const usuario = this.repo.obterUsuarioPorUserId(payload.userName);
+    if (usuario) 
+      return done(null, payload);
+    else
+      return done(new Error('User not found'), null);
+  });
+
+  constructor() {
+    passport.use(this.strategy);
+  }
+
+  initialize(): Handler {
+    return passport.initialize();
+  }
+
+  authenticate(): any {
+    return passport.authenticate("jwt", { session: false });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,19 @@ import { Logger } from 'tslog';
 import { env } from 'process';
 import { ListarProfilesController } from './controllers/listarProfileController';
 import { loginController } from './controllers/loginController';
+import { Auth } from './authentication/auth';
 
 const log = new Logger();
 const app = express();
 const PORT = env["PORT"];
+const auth = new Auth();
 
 app.use(bodyParser.json());
+app.use(auth.initialize())
 
 app.get('/', (req, res) => res.send(""));
 app.get('/login', loginController)
-app.get('/profile', ListarProfilesController);
+app.get('/profile', auth.authenticate(), ListarProfilesController);
 
 app.listen(PORT, () => {
   log.info(`Site em execução na porta ${PORT}`)
