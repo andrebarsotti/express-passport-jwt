a886b3b7e1389a85271f4a3bc51ddf4376f85765
Andre Barsotti <andre.barsotti@accelera.com.br>
2020-10-21 11:19:31 -0300

    Preparacao do ambiente

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Olá mundo!');
+import express  from 'express';
+import { Logger } from 'tslog';
+
+const log = new Logger();
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => res.send(""));
+
+app.listen(PORT, () => {
+  log.info(`Serviço em execução na porta ${PORT}`)
+});
